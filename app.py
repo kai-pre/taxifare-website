85925e5eddf9b540e7c9a3e9d9f7349960be5926
Kai Preuß <kai.preuss@protonmail.com>
2025-05-30 11:42:10 +0200

    first custom features

--- app.py
+++ app.py
@@ -1,9 +1,10 @@
 import streamlit as st
+import requests
 
 '''
 # TaxiFareModel front
 '''
-
+st.date_input()
 st.markdown('''
 Remember that there are several ways to output content into your web page...
 
@@ -22,6 +23,22 @@ Either as with the title by just creating a string (or an f-string). Or as with 
 - passenger count
 '''
 
+date = st.date_input(
+    label="Please enter a departure date: "
+    format = "YYYY/MM/DD")
+
+time = st.time_input(
+    label="Please enter a departure time: "
+    format = "")
+
+deplat = st.number_input('Please specify departure latitude: ', value = 40.75749697)
+deplon = st.number_input('Please specify departure longitude: ', value = -73.971162782)
+
+droplat = st.number_input('Please specify dropoff latitude: ', value = 40.7620270)
+dropplon = st.number_input('Please specify dropoff longitude: ', value = -73.971321)
+
+passcount = st.number_input('Please enter passenger count: ', value = 1)
+
 '''
 ## Once we have these, let's call our API in order to retrieve a prediction
 
@@ -33,11 +50,9 @@ See ? No need to load a `model.joblib` file in this app, we do not even need to 
 url = 'https://taxifare.lewagon.ai/predict'
 
 if url == 'https://taxifare.lewagon.ai/predict':
-
     st.markdown('Maybe you want to use your own API for the prediction, not the one provided by Le Wagon...')
 
 '''
-
 2. Let's build a dictionary containing the parameters for our API...
 
 3. Let's call our API using the `requests` package...
@@ -46,3 +61,11 @@ if url == 'https://taxifare.lewagon.ai/predict':
 
 ## Finally, we can display the prediction to the user
 '''
+params = {"pickup_datetime" = " ".join(date, time), "pickup_longitude" = deplon,
+            "pickup_latitude" = deplat, "dropoff_longitude" = droplon,
+            "dropoff_latitude" = droplat, "passenger_count" = passcount}
+
+apicall = requests.get(url, timeout = 10)
+
+#prediction = apicall.json()
+st.json(apicall)
