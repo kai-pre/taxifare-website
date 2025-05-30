3fe4aa4b224e3dfbd48169c667ea8a350c78903a
Kai Preuß <kai.preuss@protonmail.com>
2025-05-30 13:24:13 +0200

    final push

--- app.py
+++ app.py
@@ -1,5 +1,8 @@
 import streamlit as st
+import pandas as pd
 import requests
+import folium
+import streamlit_folium
 
 '''
 # TaxiFareModel, kai-pre version
@@ -24,6 +27,39 @@ droplon = st.number_input('Please specify dropoff longitude: ', value = -73.9713
 
 passcount = st.number_input('Please enter passenger count: ', value = 1)
 
+
+
+#mapdata = pd.DataFrame(
+#            [[deplat, deplon], [droplat, droplon]],
+#            columns=['lat', 'lon']
+#        )
+#st.map(mapdata, zoom = 12)
+
+
+# Create a Folium map
+center = [(deplat + droplat) / 2, (deplon + droplon) / 2]
+route = [[deplat, deplon], [droplat, droplon]]
+map = folium.Map(location=center, tiles="Cartodb Positron", zoom_start=13)
+
+folium.Marker([droplat, droplon], draggable = True).add_to(map)
+folium.Marker([deplat, deplon], draggable = True).add_to(map)
+
+marker1name = list(map.to_dict()["children"])[1]
+marker2name = list(map.to_dict()["children"])[2]
+
+folium.PolyLine(route,
+                color='green',
+                weight=5,
+                opacity=0.8).add_to(map)
+
+map.fit_bounds([min(route), max(route)], padding = [10, 10])
+
+imap = streamlit_folium.st_folium(map)
+
+print(imap)
+
+
+
 url = 'https://taxifare.lewagon.ai/predict'
 
 if st.button('Predict the fare'):
@@ -33,8 +69,6 @@ if st.button('Predict the fare'):
             "dropoff_latitude": droplat, "passenger_count": passcount}
 
     apicall = requests.get(url, params = params, timeout = 10)
-
-    #prediction = apicall.json()
     st.text(f"The fare for the queried taxi ride is {round(apicall.json()['fare'], 2)} dollars.")
 else:
     st.write('(Press button to predict fare)')
